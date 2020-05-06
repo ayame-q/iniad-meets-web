19257606babecc6f2a29f572cb5254bd67eaf164
Ayame <ayame.space@gmail.com>
2020-05-06 11:08:41 +0900

    公式サイトが表示されない問題の修正

--- meets/static/js/app.js
+++ meets/static/js/app.js
@@ -509,7 +509,7 @@ const showCircleInfo = (value=false) => {
 		circleInfoPanfletElement.classList.add("nodata")
 	}
 	circleInfoWebsiteAElement.href = circleInfo.website_url
-	if(circleInfo.panflet_url){
+	if(circleInfo.website_url){
 		circleInfoWebsiteElement.classList.remove("nodata")
 	} else {
 		circleInfoWebsiteElement.classList.add("nodata")
