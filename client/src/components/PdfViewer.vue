f544a8d1f61feea96c6d463881530358d2a01d9f
Ayame <ayame.space@gmail.com>
2021-03-21 03:56:55 +0900

    embedでPDFビューワを実装

---
+++ client/src/components/PdfViewer.vue
@@ -0,0 +1,42 @@
+<template>
+	<div class="pdf-viewer">
+		<embed v-bind:src="`${src}#toolbar=0&navpanes=0`">
+		<p class="close" v-on:click="$emit('close')">閉じる</p>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "PdfViewer",
+	props: {
+		src: String
+	},
+};
+</script>
+
+<style scoped>
+.pdf-viewer{
+	margin: 0;
+	padding: 0;
+	width: 100%;
+	height: 100%;
+	position: relative;
+}
+embed{
+	width: 100%;
+	height: 100%;
+	overflow-y: scroll;
+}
+.close{
+	position: absolute;
+	cursor: pointer;
+	top: 0;
+	right: 0;
+	color: #730201;
+	background-color: #ff675f;
+	border-radius: 0.5em;
+	font-size: 1.2em;
+	margin: 1em;
+	padding: 0.3em 0.5em;
+}
+</style>
