298a42366402443fac8abe1cc3409d828bee65b9
Ayame <ayame.space@gmail.com>
2021-04-01 06:52:46 +0900

    SNSシェアボタンを追加

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import VModal from 'vue-js-modal'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faTwitter, faInstagram, faLine } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faTwitter, faInstagram, faLine)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
 
 Vue.config.productionTip = false;
 
