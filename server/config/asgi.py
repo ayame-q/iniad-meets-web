275beb93067d9573411f804746bd577521145d5d
Ayame <ayame.space@gmail.com>
2021-04-22 05:42:57 +0900

    asgi, routingを戻す

--- server/config/asgi.py
+++ server/config/asgi.py
@@ -4,21 +4,9 @@ defined in the ASGI_APPLICATION setting.
 """
 
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from django.core.asgi import get_asgi_application
-#import django
-#from channels.routing import get_default_application
-import meets.routing
+import django
+from channels.routing import get_default_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
-#django.setup()
-#application = get_default_application()
-application = ProtocolTypeRouter({
-    "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        URLRouter(
-            meets.routing.websocket_urlpatterns
-        )
-    )
-})
+django.setup()
+application = get_default_application()
